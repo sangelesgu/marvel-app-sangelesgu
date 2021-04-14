9daf22740a416a3263b9d8141b1ade873377f3f2
sangelesgu <s.angelesgu@gmail.com>
2021-04-14 20:20:03 +0200

    add new directory

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -10,54 +10,48 @@ export const HeroScreen = ({ history }) => {
     history.goBack('/characters')
   }
   return (
-        <div className="container-fluid mt-2">
+    <div className="container-fluid mt-2">
 
-            {
-                loading
-                  ? (
-                        <Spinner/>
-                    )
-                  : (
-
-                        <div className="row mt-5">
-                                <>
-                                    <div className="col-6">
-                                        <img
-
-                                            alt={character.name}
-                                            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                                            className="img-thumbnail animate__animated animate__fadeInLeft"
-                                        />
-                                    </div>
-
-                                    <div className="col-6">
-                                        <h3>{character.name}</h3>
-                                        <blockquote className="blockquote text-right">
-                                            <p className="font-italic mb-0">{character.description}</p>
-                                        </blockquote>
-
-                                        <ul>
-                                        </ul>
-
-                                        <button className="btn btn-outline-danger"
-                                            onClick={handleReturn}>
-                                         <b>Back</b>
-                                        </button>
-
-                                        <a
-                                            href={character.urls[0].url}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                            className="m-2"
-                                        >
-                                            <button className="btn btn-outline-secondary ">
-                                                <b>Details</b>
-                                            </button>
-                                        </a>
-                                    </div>
-                                </>
+        {
+          loading
+            ? (
+                <Spinner/>
+              )
+            : (
+                <div className="row mt-5">
+                    <>
+                      <div className="col-6">
+                          <img
+                                alt={character.name}
+                                 src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                                 className="img-thumbnail animate__animated animate__fadeInLeft"
+                           />
                         </div>
-                    )
+
+                        <div className="col-6">
+                          <h3>{character.name}</h3>
+                              <blockquote className="blockquote text-right">
+                                 <p className="font-italic mb-0">{character.description}</p>
+                              </blockquote>
+
+                              <button className="btn btn-outline-danger"
+                                  onClick={handleReturn}>
+                                 <b>Back</b>
+                               </button>
+                                <a
+                                  href={character.urls[0].url}
+                                  target="_blank"
+                                  rel="noreferrer"
+                                  className="m-2"
+                                >
+                                    <button className="btn btn-outline-secondary ">
+                                        <b>Details</b>
+                                    </button>
+                                </a>
+                        </div>
+                   </>
+                </div>
+              )
             }
         </div>
   )
